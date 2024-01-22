9d2aa4d5b0d7d6ce8860095bae243b25ec128b32
Willson Guayanay <wilson.guayanay@epn.edu.ec>
2024-01-22 07:52:38 -0500

    Cambios en HomeScreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export const Home = ({ navigation }) => {
     return <View style={styles.container}>
-        <Text>HOME</Text>
+        <Text>Bienvenido, soy el estudiante 1</Text>
 
         <View style={styles.botones}>
 
@@ -33,7 +33,7 @@ export const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'red',
         alignItems: 'center',
         justifyContent: 'center',
     },
